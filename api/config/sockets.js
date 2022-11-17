3d19e2fc8a649c04874f72277ed4e03d41772cd0
José Braga <jj.braga@fct.unl.pt>
2022-11-17 11:14:07 +0000

    Just removed comments from sockets.js.

--- api/config/sockets.js
+++ api/config/sockets.js
@@ -9,35 +9,9 @@ const corsOptions = {
         }
 };
 
-//var io = (server) => socket_io(server, corsOptions);
-//var sockets = {};
-
 var history = [];
 let myIO;
-//var clients = [];
 
-//sockets.io = io;
-/*
-io.on('connection', function (socket){
-    //console.log('A user connected');
-    //console.log(socket.client.id);
-    socket.emit('message_all', history);
-});
-
-sockets.adminMessageAll = function (message) {
-    history.push({msg: message, time: time.momentToDate(time.moment(),undefined,'YYYY-MM-DD HH:mm:ss')});
-    io.sockets.emit('message_all', history);
-};
-
-sockets.adminMessagesClear = function (option) {
-    history = [];
-    io.sockets.emit('message_all', history);
-};
-
-sockets.getServerMessages = function () {
-    return history;
-};
-*/
 module.exports.io = (server) => {
     let io = socket_io(server, corsOptions);
     io.on('connection', function (socket){
